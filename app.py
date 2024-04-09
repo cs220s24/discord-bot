2d089adf08cab68288a50892179e657595bc2d77
Kaleb Missmer <missmerk@moravian.edu>
2024-04-08 23:48:17 -0400

    Added greeting feature

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# This example requires the 'message_content' intent.
 import discord
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 token = os.getenv('BOT_TOKEN')
 
 
@@ -14,8 +14,16 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello!')
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 
+
 client = MyClient(intents=intents)
 client.run(token)
+
+
