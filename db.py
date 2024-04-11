9ae4a71c5accdc3caf9f2ca6912ab91f15c02c83
Deni Velasquez <velasquezd@moravian.edu>
2024-04-10 21:19:53 -0400

    re-added method

--- db.py
+++ db.py
@@ -5,6 +5,11 @@ class RedisHandler:
     def __init__(self, host='localhost', port=6379, db=0):
         self.redis_client = redis.StrictRedis(host=host, port=port, db=db)
 
+    def set_motivational_quotes(self, quotes):
+        """Set the motivational quotes in the Redis database."""
+        for index, quote in enumerate(quotes):
+            self.redis_client.set(f'motivational_quote:{index}', quote)
+
     def get_random_quote(self):
         """Retrieve a random motivational quote from the Redis database."""
         keys = self.redis_client.keys('motivational_quote:*')
