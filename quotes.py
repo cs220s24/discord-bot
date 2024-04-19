171dfc29829a397dd021938e8ceec3889bd39b73
kmissmer <143435924+kmissmer@users.noreply.github.com>
2024-04-18 21:54:51 -0400

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -2,5 +2,33 @@ MOTIVATIONAL_QUOTES = [
     "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
     "Hardships often prepare ordinary people for an extraordinary destiny. - C.S. Lewis",
     "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "It does not matter how slowly you go as long as you do not stop. - Confucius",
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+    "In the middle of every difficulty lies opportunity. - Albert Einstein",
+    "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
+    "You miss 100% of the shots you don't take. - Wayne Gretzky"
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
 ]
