7fd9e4488a97e9577770d5822a5e3c93c443eb84
Brian Demyan <59424740+elon-shmusk@users.noreply.github.com>
2024-04-29 21:18:24 -0400

    Update dock_aws.sh

--- dock_aws.sh
+++ dock_aws.sh
@@ -1,3 +1,3 @@
 cd ~/discord-bot
-sudo docker run --name redisdb --network=botdb -d -p 6379:6379 -v $(pwd)/data:/data redis
-sudo docker run -it --name discord-bot --network=botdb discord-bot
+docker run --name redisdb --network=botdb -d -p 6379:6379 -v $(pwd)/data:/data redis
+docker run -it --name discord-bot --network=botdb discord-bot
